0152782222161b97e32e8e357ac74b30ac454c05
Bedanta Bikash Dutta <144888311+proxybinder@users.noreply.github.com>
2026-07-27 01:10:57 +0530

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int acc = 0;
+
+        for(int i=0; i<nums.length; i++){
+            acc ^= nums[i];
+        }
+        return acc;
+    }
+}
